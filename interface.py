3d95485ab2eeb2f694674f6d3476feb7c9a1adea
Will Robinson <39654477+wrobbo@users.noreply.github.com>
2019-05-24 17:46:27 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'gradingwindow2.ui'
-#
-# Created by: PyQt5 UI code generator 5.11.3
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_GradingWindow(object):
